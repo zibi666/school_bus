2a352dde468eefc18f429f5d7b4efac5c6c49553
子必 <1925328075@qq.com>
2025-12-08 18:58:30 +0800

    修改后端地址接口ip

--- code/web/school_bus/src/api/index.js
+++ code/web/school_bus/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://192.168.1.108:9090/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://118.195.133.25:9090/api',
   timeout: 5000
 })
 
