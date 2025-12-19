cdccb4bbc87408246ab429879002a5077c0dd8d2
子必 <1925328075@qq.com>
2025-12-19 15:16:27 +0800

    删除部分markdown

--- code/web/school_bus/vite.config.js
+++ code/web/school_bus/vite.config.js
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: 'localhost',
+    port: 8080,
+  },
   plugins: [
     vue(),
     vueDevTools(),
